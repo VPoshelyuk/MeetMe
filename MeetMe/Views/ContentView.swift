5bdce6fd1438aa7d1cebd5427dbd7fe9cfc814ba
VPoshelyuk <aslava198@gmail.com>
2020-07-12 10:06:03 -0400

    Initial commit: Static profile page created, Segment control view

---
+++ MeetMe/Views/ContentView.swift
@@ -0,0 +1,179 @@
+//
+//  ContentView.swift
+//  MeetMe
+//
+//  Created by Slava Pashaliuk on 7/12/20.
+//  Copyright © 2020 Viachaslau Pashaliuk. All rights reserved.
+//
+
+import SwiftUI
+import MessageUI
+
+struct ContentView: View {
+    enum ActiveSheet: Hashable, Identifiable {
+       case web, mail
+        
+        var id: Int {
+            return self.hashValue
+        }
+    }
+    
+    @Environment(\.colorScheme) var colorScheme
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var presentingModal = false
+    @State private var activeSheet: ActiveSheet? = nil
+    @State var link = ""
+    var links = ["https://www.facebook.com/slava.poshelyk", "https://twitter.com/SPoshelyk", "https://www.linkedin.com/in/viachaslau-pashaliuk/", "https://github.com/VPoshelyuk/", "https://www.linkedin.com/in/viachaslau-pashaliuk/"]
+    @State private var selected = 1
+    var body: some View {
+        VStack {
+            if self.selected == 0 {
+                Spacer()
+                Text("Hi, asshole!")
+                Spacer()
+            }else if self.selected == 1 {
+                Spacer()
+                Image("test")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                    .clipShape(Circle())
+                    .overlay(Circle().strokeBorder(Color.gray, lineWidth: 3))
+                Text("Viachaslau Pashaliuk")
+                    .font(.custom("Ubuntu-Bold", size: 34))
+                HStack(spacing: 20) {
+                    Image("facebook")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture { self.presentingModal = true; self.activeSheet = .web; self.link =  "https://www.facebook.com/slava.poshelyk"}
+                    Image("twitter")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture { self.presentingModal = true; self.activeSheet = .web; self.link =  "https://twitter.com/SPoshelyk"}
+                    Image("linkedin")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture { self.presentingModal = true; self.activeSheet = .web; self.link =  "https://www.linkedin.com/in/viachaslau-pashaliuk/"}
+                    Image("github")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture { self.presentingModal = true; self.activeSheet = .web; self.link =  "https://github.com/VPoshelyuk/"}
+                    Image("email")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture { self.activeSheet = .mail; self.presentingModal = true }
+                    Image("portfolio")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 50, height: 50)
+                        .onTapGesture {self.activeSheet = .web; self.link =  "https://www.linkedin.com/in/viachaslau-pashaliuk/"; self.presentingModal = true; print(self.link) }
+                }
+                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                .disabled(!MFMailComposeViewController.canSendMail())
+                .sheet(item: $activeSheet) { item in
+                    if item == .web {
+                        ModalView(presentedAsModal: self.$presentingModal, URL: self.$link)
+                    }else if item == .mail {
+                        MailView(result: self.$result, toRecipents: ["v.pashaliuk@gmail.com"])
+                    }
+                }
+                Divider()
+                .background(Color.gray)
+                .frame(width: UIScreen.main.bounds.width - 30)
+                Text("About")
+                    .font(.custom("Ubuntu-Bold", size: 34))
+                    .multilineTextAlignment(.center)
+                Divider()
+                    .background(Color.gray)
+                    .frame(width: UIScreen.main.bounds.width - 30)
+                ScrollView(){
+                    Text("First things first, I like everything about computers, especially the fact that they are good at following instructions. It was fascinating to me since early childhood to see how machines are able to do so much just switching between 0's and 1's. My actual programming experience started with a calculator app that was written in Objective-C and it still can be found somewhere on my GitHub.\nDuring my studying journey, I had a chance to attend Flatiron School's Software Engineering Immersive program, where I learned the tools for Full-Stack Web-Development, such as Ruby on Rails for Backend and JavaScript with React and Redux frameworks for Frontend. Studying at Flatiron School included a lot of collaboration with other students which I enjoyed since working on a team is a great opportunity to share your knowledge and learn from others.")
+                        .font(.custom("Ubuntu-Light", size: 18))
+                        .multilineTextAlignment(.center)
+                }
+            }else {
+                Spacer()
+                Text("Bye, asshole!")
+                Spacer()
+            }
+            SegmentedControlView(selected: self.$selected)
+                .padding(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ModalView: View {
+    @Binding var presentedAsModal: Bool
+    @Binding var URL: String
+    var body: some View {
+        VStack {
+            HStack{
+                Button("Back") { self.presentedAsModal = false }
+            }
+            .frame(maxHeight: 12.0)
+            .padding()
+            DetailView(url: URL)
+        }
+    }
+}
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Environment(\.presentationMode) var presentation
+    @Binding var result: Result<MFMailComposeResult, Error>?
+    var toRecipents = [String]()
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var presentation: PresentationMode
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        var toRecipents = [String]()
+
+        init(presentation: Binding<PresentationMode>,
+             result: Binding<Result<MFMailComposeResult, Error>?>, toRecipents: [String]) {
+            _presentation = presentation
+            _result = result
+            self.toRecipents = toRecipents
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                $presentation.wrappedValue.dismiss()
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(presentation: presentation,
+                           result: $result, toRecipents: toRecipents)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.setToRecipients(toRecipents)
+        vc.mailComposeDelegate = context.coordinator
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+
+    }
+}
---
+++ MeetMe/Views/SegmentedControlView.swift
@@ -0,0 +1,141 @@
+//
+//  SegmentedControlView.swift
+//  MeetMe
+//
+//  Created by Slava Pashaliuk on 7/12/20.
+//  Copyright © 2020 Viachaslau Pashaliuk. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    func eraseToAnyView() -> AnyView {
+        AnyView(self)
+    }
+}
+
+struct SizePreferenceKey: PreferenceKey {
+    typealias Value = CGSize // Define that the PreferenceKey attribute is of type CGSize
+    static var defaultValue: CGSize = .zero // Default to zero size
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
+
+struct BackgroundGeometryReader: View {
+    var body: some View {
+        GeometryReader { geometry in
+            return Color
+                    .clear
+                    .preference(key: SizePreferenceKey.self, value: geometry.size)
+        }
+    }
+}
+
+struct SizeAwareViewModifier: ViewModifier {
+    
+    @Binding private var viewSize: CGSize
+
+    init(viewSize: Binding<CGSize>) {
+        self._viewSize = viewSize
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .background(BackgroundGeometryReader())
+            .onPreferenceChange(SizePreferenceKey.self, perform: { if self.viewSize != $0 { self.viewSize = $0 }})
+    }
+}
+
+struct SegmentedControlView: View {
+    private static let ActiveSegmentColor: Color = Color(.tertiarySystemBackground)
+    private static let BackgroundColor: Color = Color(.secondarySystemBackground)
+    private static let ShadowColor: Color = Color.black.opacity(0.2)
+    private static let TextColor: Color = Color(.secondaryLabel)
+    private static let SelectedTextColor: Color = Color(.label)
+
+    private static let TextFont: Font = .system(size: 14)
+    
+    private static let SegmentCornerRadius: CGFloat = 12
+    private static let ShadowRadius: CGFloat = 4
+    private static let SegmentXPadding: CGFloat = 16
+    private static let SegmentYPadding: CGFloat = 8
+    private static let PickerPadding: CGFloat = 4
+    
+    private static let AnimationDuration: Double = 0.3
+    
+    @State private var segmentSize: CGSize = .zero
+    @Binding private var selected: Int
+    
+    init(selected: Binding<Int>) {
+        self._selected = selected
+    }
+    
+    private let picsText = [(image: "cards_stack", text: "Cards"),(image: "profile", text: "Me"),(image: "settings", text: "Settings")]
+    
+    private var activeSegmentView: AnyView {
+        let isInitialized: Bool = segmentSize != .zero
+        if !isInitialized { return EmptyView().eraseToAnyView() }
+        return
+            RoundedRectangle(cornerRadius: SegmentedControlView.SegmentCornerRadius)
+                .foregroundColor(SegmentedControlView.ActiveSegmentColor)
+                .shadow(color: SegmentedControlView.ShadowColor, radius: SegmentedControlView.ShadowRadius)
+                .frame(width: self.segmentSize.width, height: self.segmentSize.height)
+                .offset(x: self.computeActiveSegmentHorizontalOffset(), y: 0)
+                .animation(Animation.linear(duration: SegmentedControlView.AnimationDuration))
+                .eraseToAnyView()
+    }
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            self.activeSegmentView
+            HStack {
+                ForEach(0..<self.picsText.count, id: \.self) { index in
+                  self.getSegment(for: index)
+                }
+            }
+        }
+        .padding(SegmentedControlView.PickerPadding)
+        .background(SegmentedControlView.BackgroundColor)
+        .clipShape(RoundedRectangle(cornerRadius: SegmentedControlView.SegmentCornerRadius))
+    }
+    
+    private func computeActiveSegmentHorizontalOffset() -> CGFloat {
+        CGFloat(self.selected) * (self.segmentSize.width + SegmentedControlView.SegmentXPadding / 2)
+    }
+    
+    private func getSegment(for index: Int) -> some View {
+        guard index < self.picsText.count else {
+            return EmptyView().eraseToAnyView()
+        }
+        let isSelected = self.selected == index
+        return
+            HStack{
+                Image(picsText[index].image)
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 25, height: 25)
+                Text(picsText[index].text)
+                    .font(SegmentedControlView.TextFont)
+            }
+            .foregroundColor(isSelected ? SegmentedControlView.SelectedTextColor: SegmentedControlView.TextColor)
+            .lineLimit(1)
+            .padding(.vertical, SegmentedControlView.SegmentYPadding)
+            .padding(.horizontal, SegmentedControlView.SegmentXPadding)
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .modifier(SizeAwareViewModifier(viewSize: self.$segmentSize))
+            .onTapGesture { self.onTap(index) }
+            .eraseToAnyView()
+    }
+    
+    private func onTap(_ index: Int) {
+        self.selected = index
+    }
+}
+
+struct SegmentedControlView_Previews: PreviewProvider {
+    @State static var selected = 0
+    static var previews: some View {
+        SegmentedControlView(selected: $selected)
+    }
+}
